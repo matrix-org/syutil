a65b4a25718c71f4abaca2bfca5a312d219d67b5
Mark Haines <mark.haines@matrix.org>
2015-04-21 15:53:37 +0100

    Bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(fname):
 
 setup(
     name="syutil",
-    version="0.0.4",
+    version="0.0.5",
     packages=find_packages(exclude=["tests"]),
     description="Synapse Matrix Server Utilities",
     install_requires=[
